cbc3858e701811c272d9c0b9ef1f76e2a734ab07
Beni <braunger@zib.de>
2025-05-09 23:58:26 +0200

    btrfs: use only logger to print

--- filesystems/btrfs.go
+++ filesystems/btrfs.go
@@ -41,7 +41,7 @@ func PruneExpiredSnapshots(path string) ([]string, error) {
 	b, _ := btrfs.Open(path, false)
 	subvols, _ := b.ListSubvolumes(func(svi btrfs.SubvolInfo) bool {
 				if svi.RootID == 5 {
-					fmt.Println("Refusing to work on btrfs <FS_TREE>")
+					log.Debug("Refusing to work on btrfs <FS_TREE>")
 					return false
 				}
 				return true
@@ -51,7 +51,7 @@ func PruneExpiredSnapshots(path string) ([]string, error) {
 		fullPath := filepath.Join(path, sv.Path)
 		xattr, err := xattr.Get(fullPath, "user.expire")
 		if err != nil {
-			log.Error(fmt.Sprintf("PruneExpiredSnapshots: %w", err))
+			log.Debug(fmt.Sprintf("PruneExpiredSnapshots: %w", err))
 			continue
 		}
 		t, err := time.Parse(TimeFormat, string(xattr))
@@ -60,10 +60,7 @@ func PruneExpiredSnapshots(path string) ([]string, error) {
 		}
 		if t.Before(time.Now()) {
 			expiredSubs = append(expiredSubs, sv)
-			fmt.Print("  '")
-			fmt.Print(fullPath)
-			fmt.Print("' expired since ")
-			fmt.Println(t.Format(TimeFormat))
+			log.Info(fmt.Sprintf("↳ '%s' expired since %s", fullPath, t.Format(TimeFormat)))
 			btrfs.DeleteSubVolume(fullPath)
 		}
 	}
