9c4f0c892075c612739197f2b64672b06adac337
Beni <braunger@zib.de>
2025-09-23 00:00:20 +0200

    btrfs: rewrite pruning to use correct subvolume paths
    added findParentMount function
    added cleanPath function

--- filesystems/btrfs.go
+++ filesystems/btrfs.go
@@ -17,15 +17,19 @@ import (
 	"errors"
 	"fmt"
 	"github.com/dennwc/btrfs"
+	"github.com/moby/sys/mountinfo"
 	"github.com/pkg/xattr"
 	"github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 	"time"
 	"xpire/pluginapi"
 )
 
 const TimeFormat = time.DateTime
+const RC_OK = 0
 const RC_ERR_PLUGIN = 7
 
 var (
@@ -35,6 +39,36 @@ var (
 type BtrfsPlugin struct{}
 
 // ---- internal functions
+func cleanPath(path string) (string, error) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+	absPath, err = filepath.EvalSymlinks(absPath)
+	if err != nil {
+		return "", err
+	}
+	absPath = filepath.Clean(absPath)
+	return absPath, nil
+}
+
+func findParentMount(path string) (string, error) {
+	absPath, _ := cleanPath(path)
+	log.Debug(fmt.Sprintf("searching for parent mount of '%s'", absPath))
+	// get all possible parents mounts
+	mounts, _ := mountinfo.GetMounts(mountinfo.ParentsFilter(absPath))
+	if len(mounts) == 0 {
+		return "", fmt.Errorf("no parent mounts found for %s", absPath)
+	}
+	if len(mounts) > 1 {
+		// usually we find at least two parents ;-)
+		sort.Slice(mounts, func(i, j int) bool {
+			return len(mounts[i].Mountpoint) > len(mounts[j].Mountpoint)
+		})
+	}
+	log.Debug(fmt.Sprintf("found parent mount '%s'", mounts[0].Mountpoint))
+	return mounts[0].Mountpoint, nil
+}
 
 // check if xpire runs as root
 func isRoot() bool {
@@ -53,7 +87,7 @@ func (p BtrfsPlugin) SetExpireDate(t time.Time, path string) error {
 	//FIXME: check XATTR_SUPPORTED first
 	isSubVolume, _ := btrfs.IsSubVolume(path)
 	if isSubVolume == false {
-		errorMsg := errors.New(fmt.Sprintf("'%s' is not a btrfs subvolume", path))
+		errorMsg := fmt.Errorf("'%s' is not a btrfs subvolume", path)
 		return errorMsg
 	}
 	if err := xattr.Set(path, "user.expire", []byte(t.Format(TimeFormat))); err != nil {
@@ -67,25 +101,52 @@ func (p BtrfsPlugin) PruneExpired(path string) ([]string, error) {
 		return nil, errors.New("btrfs plugin needs root permissions to list all subvolumes")
 	}
 	log.Info(fmt.Sprintf("pruning expired data in '%s'", path))
-	b, _ := btrfs.Open(path, false)
+
+	// next parent mountpoint of path is the btrfs filesystem we work on
+	absPath, _ := cleanPath(path)
+	var mountPoint string = absPath
+	pathIsMountpoint, err := mountinfo.Mounted(absPath)
+	if pathIsMountpoint == false {
+		mountPoint, _ = findParentMount(absPath)
+	}
+	b, err := btrfs.Open(mountPoint, false)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot open btrfs filesystem\n%w", err)
+	}
+
+	// remove mountpoint path from given path to guess the subvolume names
+	var relPath string = strings.Replace(absPath, mountPoint, "", 1)
+	relPath = strings.TrimLeft(relPath, "/")
+	pathIsSubvolume, _ := btrfs.IsSubVolume(absPath)
+	if pathIsSubvolume == true {
+		pathVolume, _ := b.SubvolumeByPath(absPath)
+		log.Debug(fmt.Sprintf("Subvolume ID of '%s' is '%d'", absPath, pathVolume.RootID))
+	}
 	subvols, _ := b.ListSubvolumes(func(svi btrfs.SubvolInfo) bool {
 		if svi.RootID == 5 {
 			log.Debug("Refusing to work on btrfs <FS_TREE>")
 			return false
 		}
-		return true
+		if strings.HasPrefix(svi.Path, relPath) {
+			return true
+		}
+		return false
 	})
 	var expiredSubs []btrfs.SubvolInfo
 	for _, sv := range subvols {
-		fullPath := filepath.Join(path, sv.Path)
+		// FIXME: BUG! This is wrong if the path is not the mountpoint of btrfs FS
+		// when path is a subvolume, then we need to filter the all subvolumes of b and check if they start with name of the given subvolume
+		fullPath := filepath.Join(mountPoint, sv.Path)
+		log.Debug(fmt.Sprintf("Working on path '%s'", fullPath))
 		xattr, err := xattr.Get(fullPath, "user.expire")
 		if err != nil {
-			log.Debug(fmt.Sprintf("Function PruneExpired: %w", err))
+			log.Debug(fmt.Errorf("Cannot read expire xattr on '%s'\n\t%w", fullPath, err))
 			continue
 		}
 		t, err := time.Parse(TimeFormat, string(xattr))
 		if err != nil {
-			return nil, err
+			log.Debug(fmt.Errorf("Cannot parse expire date format:\n\t%w", err))
+			continue
 		}
 		if t.Before(time.Now()) {
 			expiredSubs = append(expiredSubs, sv)
