e8201379cf8eba714a6eba883464bba48747c4da
Beni <braunger@zib.de>
2025-09-23 00:00:36 +0200

    tests: setup more btrfs cases

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+
+#### BTRFS
+
 # prepare btrfs
 mkdir -p ./mnt/btrfs
 truncate --size 5G btrfs.img
 mkfs.btrfs btrfs.img &>/dev/null
 mount -v -o loop btrfs.img ./mnt/btrfs
+
 btrfs subvolume create ./mnt/btrfs/subvolume
 btrfs subvolume snapshot ./mnt/btrfs/subvolume ./mnt/btrfs/snapshot
 chmod 777 ./mnt/btrfs -R
 btrfs subvolume create ./mnt/btrfs/root-only
 chmod 640 ./mnt/btrfs/root-only
 chown root: ./mnt/btrfs/root-only
+
+btrfs subvolume create ./mnt/btrfs/wrong-time-format
+setfattr ./mnt/btrfs/wrong-time-format/ -n user.expire -v "205-02 111"
+
 mkdir -p mnt/btrfs/dir
