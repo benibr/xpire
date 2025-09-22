3f678a65cf1f516a943dab02027ba52079cdb62e
Beni <braunger@zib.de>
2025-09-22 15:33:57 +0200

    tests: add root-only dir for btrfs

--- tests/setup.sh
+++ tests/setup.sh
@@ -8,4 +8,7 @@ mount -v -o loop btrfs.img ./mnt/btrfs
 btrfs subvolume create ./mnt/btrfs/subvolume
 btrfs subvolume snapshot ./mnt/btrfs/subvolume ./mnt/btrfs/snapshot
 chmod 777 ./mnt/btrfs -R
+btrfs subvolume create ./mnt/btrfs/root-only
+chmod 640 ./mnt/btrfs/root-only
+chown root: ./mnt/btrfs/root-only
 mkdir -p mnt/btrfs/dir
