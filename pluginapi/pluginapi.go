5939109671e1b24ff27f63fab59ff57402b60431
Beni <braunger@zib.de>
2026-01-07 18:59:22 +0100

    pluginapi: overhauled Prune definition

--- pluginapi/pluginapi.go
+++ pluginapi/pluginapi.go
@@ -9,26 +9,32 @@ import (
 // plugin must implement.
 
 type FsPluginApi interface {
-	// InitLogger gets called right after plugin initialisation
+	// InitLogger gets called right after plugin initialization
 	// to pass the current logrus instance point to the plugin
 	InitLogger(l *logrus.Logger) error
 
-	// SetExpireDate is used to set the expiry date on a given
-	// file/folder/subset. The function must overwrite the date
-	// without asking in case it's already set.
-	// Permission checks must be done in plugin.
+	// SetExpireDate is used to set the expire date on a given
+	// file/folder/subset.
+	// Must overwrite the date without asking in case it's already set.
+	// Return only errors
 	SetExpireDate(t time.Time, path string) error
 
-	// UnsetExpireDate is used to remove a expiry date on a given
+	// UnsetExpireDate is used to remove a expire date on a given
 	// file/folder/subset.
+	// Must overwrite the date without asking in case it's already set.
+	// Return only errors
 	UnsetExpireDate(path string) error
 
 	// PruneExpired cleans up all expired files/folders/subsets
-	// under the given path recursivly without any further user interaction.
-	// Permission checks must be done in plugin.
-	PruneExpired(path string) ([]string, error)
+	// Gets a list of absoulte paths that have a expire date set (eg. found by List)
+	// Must check if a path is expired or not
+	// Must check/handle permissions.
+	// Returns only errors.
+	PruneExpired(paths []string) error
 
-	// List all set expire dates that are currently set
+	// List all expire dates that are currently set
 	// regardless if they're reached or not
+	// Returns a list of all absolute paths that have
+	// a expire date set.
 	List(path string) ([]string, error)
 }
